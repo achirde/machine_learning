398dcca11a23c88081e65da2bc50d0d1923b8ced
achirde <dev.achir@gmail.com>
2019-04-21 03:39:30 -0500

    Deloitte Competetion

--- Deloitte/competetion_01/bitcoin.py
+++ Deloitte/competetion_01/bitcoin.py
@@ -10,12 +10,13 @@ def measure_accuracy(y_true, y_pred):
     mse = np.array([])
 
     for i in range(len(y_true)):
-        diff = (y_true['close'][i] - y_pred['close'][i])**2
+        diff = ((y_true['close'][i] - y_pred['close'][i])/y_true['close'][i])**2
         mse = np.append(mse, diff)
     return mse.mean()
 
 
 data = pd.read_csv('files/train_timeseries2.csv', index_col=0)
+data_test = pd.read_csv('files/test_timeseries2.csv', index_col=0)
 
 
 # print(data.head())
@@ -36,11 +37,16 @@ score = reg.score(X_test, y_test)
 
 y_pred_df = pd.DataFrame(y_pred, columns=['close'], index=y_test.index)
 
+y_sub = reg.predict(data_test)
+y_sub = y_sub.round(decimals=2)
+y_sub_df = pd.DataFrame(y_sub, columns=['close'], index=data_test.index)
+
 score = mean_squared_error(y_test, y_pred)
 score = measure_accuracy(y_test, y_pred_df)
 
 
 y_pred_df.to_csv('./files/y_predicted.csv')
 y_test.to_csv('./files/y_test.csv')
+y_sub_df.to_csv('./files/test_submission2.csv')
 print(score)
 
