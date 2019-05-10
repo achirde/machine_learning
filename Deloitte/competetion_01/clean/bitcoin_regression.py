8740633ef4fb80f16a880c8727cb69416f76ceb1
achirde <dev.achir@gmail.com>
2019-05-10 07:29:35 -0500

    update clean files

--- Deloitte/competetion_01/clean/bitcoin_regression.py
+++ Deloitte/competetion_01/clean/bitcoin_regression.py
@@ -9,8 +9,11 @@ from sklearn.svm import SVR
 
 # File Names 
 TRAIN_TIMESERIES = 'input_csv_files/train_timeseries2.csv'
+TEST_TIMESERIES  = 'input_csv_files/test_timeseries2.csv'
 TRAIN_TWEETER    = 'input_csv_files/train_tweets.csv'
-TEST_TWEETER    = 'input_csv_files/test_tweets.csv'
+TEST_TWEETER     = 'input_csv_files/test_tweets.csv'
+
+SUB_FILE_NAME    = 'output_files/SUBMISSION_GB_HUBER_FMSE_32.csv'
 
 # Model Selection 
 model = ['r', 'l', 'g', 's']
@@ -116,10 +119,79 @@ def process_tweet_data(file):
 train_tweet_data = process_tweet_data(TRAIN_TWEETER)
 test_tweet_data  = process_tweet_data(TEST_TWEETER)
 
-print(train_tweet_data.head())
+# print(train_tweet_data.head())
 
 # Merge Tweet Data with Timeseries Data
 data_close = data[columns]
-print(data_close.head())
-# data_w_tweets = train_tweet_data.merge(data_close, left_index=True, right_index=True)
-# data_w_tweets.head()
+# print(data_close.head())
+data_w_tweets = train_tweet_data.merge(data_close, left_index=True, right_index=True)
+# print(data_w_tweets.head())
+
+if ENABLE_TWEET:
+    data = data_w_tweets
+
+print(len(data))
+
+# data[data.isnull().any(axis=1)]
+data = data.dropna(how='any')
+
+# Split Training data
+data_train = data.sample(frac=0.8, random_state=0)
+data_test = data.drop(data_train.index)
+
+# train_stat = data_train.describe()
+# train_stat.pop('close')
+# train_stat = train_stat.transpose()
+
+y_train = data_train.pop("close")
+y_test  = data_test.pop("close")
+
+
+# sc = StandardScaler()
+# normed_X_train = sc.fit_transform(data_train)
+# normed_X_test = sc.transform(data_test)
+
+normed_X_train = data_train
+normed_X_test  = data_test
+
+# Model Fitting
+reg = GradientBoostingRegressor(loss=LOSS, criterion = CRITERION, max_depth=MAX_DEPTH)
+  
+
+reg.fit(normed_X_train, y_train)
+
+y_pred = reg.predict(normed_X_test)
+y_pred = y_pred.round(decimals=2)
+score = reg.score(normed_X_test, y_test)
+
+print(score)
+
+y_pred_df = pd.DataFrame(y_pred, columns=['close'], index=y_test.index)
+
+# Prepare Submission File
+
+columns.remove('close')
+data_sub = pd.read_csv(TEST_TIMESERIES, index_col=0)
+data_sub = data_sub[columns]
+
+data_close_sub = data_sub[columns]
+data_w_tweets_sub = test_tweet_data.merge(data_close_sub, left_index=True, right_index=True)
+
+# Enable Tweet Data
+if ENABLE_TWEET:
+  data_sub = data_w_tweets_sub  
+
+data_sub = data_sub.dropna(how='any')
+
+# normalized_sub = sc.transform(data_sub)
+normalized_sub = data_sub
+
+# Submission Prediction
+y_sub = reg.predict(normalized_sub)
+y_sub = y_sub.round(decimals=2)
+y_sub_df = pd.DataFrame(y_sub, columns=['close'], index=data_sub.index)
+y_sub_df = y_sub_df.groupby('date').agg('mean')
+
+print(y_sub_df.head())
+
+y_sub_df.to_csv(SUB_FILE_NAME)
